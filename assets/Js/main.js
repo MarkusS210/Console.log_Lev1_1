ce9847db7157237a6e2056990e5bb0783e910878
Markus <Markus.Schneiderm3@googlemail.com>
2023-01-03 13:32:52 +0100

    Console.log_Lev1_1

---
+++ assets/Js/main.js
@@ -0,0 +1,27 @@
+a = 12;
+b = a * 3;
+console.log(b);
+
+document.write (b)
+
+
+console.log("Hello World");
+
+
+
+console.log("Anton");
+
+const firstName = "Anton" 
+console.log("firstName");
+
+let age = 28
+console.log(age);
+
+let job = "Trainer"
+console.log(job)
+
+let married = "true"
+console.log(married);
+
+console.log(firstName + " ist " + age + "Jahre alt, " + "von Beruf " + job + " und ist verheiratet:" + married );
+
